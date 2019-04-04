2ef57b8007c08df6f2f166da1cfe8ca2ceb3cf30
bbjonz <32881398+bbjonz@users.noreply.github.com>
2019-04-04 15:57:12 -0700

    Add files via upload

---
+++ R/data-desc.R
@@ -0,0 +1,19 @@
+#' lag.sim--simulated sequential data
+#'
+#' Data were simulated based on code found at:
+#' https://stephens999.github.io/fiveMinuteStats/simulating_discrete_chains_1.html
+#'
+#' @docType data
+#'
+#' @usage data(lag.sim)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references https://stephens999.github.io/fiveMinuteStats/simulating_discrete_chains_1.html
+#'#'
+#' @examples
+#' data(lag.sim)
+#' 
+"lag.sim"
