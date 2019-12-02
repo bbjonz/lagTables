2d66a41beb435705455abb81d0e7d9907e9f85d2
bbjonz <32881398+bbjonz@users.noreply.github.com>
2019-12-02 16:41:22 -0700

    Add files via upload

--- R/lagTables.R
+++ R/lagTables.R
@@ -69,7 +69,6 @@ trprobs <- function(d, lagvar, laggroup=NULL, title="Lag Sequential Descriptive 
     #see last comment in: https://stackoverflow.com/questions/45206908/kableextra-dynamic-add-header-above-labeling
     groupnames <- sub("(.)", "\\U\\1", groupnames, perl=TRUE)
     tablecols <- rep(numcodes,numgroupcodes)
-    print(tablecols)
     names(tablecols) <- c(groupnames)
     
     lagdat <- d %>%
@@ -120,6 +119,8 @@ trprobs <- function(d, lagvar, laggroup=NULL, title="Lag Sequential Descriptive 
       dplyr::tbl_df() %>% 
       as.data.frame() -> top.dat
     
+    print(stats.out)
+    
     stats.out %>%
       dplyr::select(lag1, raw, Condition, tpsres) %>%
       reshape2::acast(., raw ~ lag1 ~ Condition ) %>%
@@ -130,11 +131,14 @@ trprobs <- function(d, lagvar, laggroup=NULL, title="Lag Sequential Descriptive 
     lagout <- cbind(rep(codenames, 2),lagout)
     
     lagout %>%
-      knitr::kable(caption = paste0(title, ", Lag = ", lagnum), digits=2, format = "html", escape = F, align=rep('c', numcodes), col.names = c("",rep(codenames,numgroupcodes))) %>%
+      knitr::kable(caption = paste0(title, ", Lag = ", lagnum), digits=2, 
+                   format = "html", escape = F, align=rep('c', numcodes), 
+                   col.names = c("",rep(codenames,numgroupcodes))) %>%
       kableExtra::kable_styling(bootstrap_options = "striped", full_width = FALSE)  %>%
       kableExtra::add_header_above(c("", tablecols)) %>%
-      kableExtra::group_rows("Observed Frequencies\n(Expected Frequencies)", 1, numcodes) %>%
-      kableExtra::group_rows("Standardized Residuals\n(Transitional Probablities)", numcodes+1, numcodes*2)
+      kableExtra::pack_rows("Observed Frequencies\n(Expected Frequencies)", 1, numcodes) %>%
+      kableExtra::pack_rows("Transitional Probablities\n(Standardized Residuals)", 
+                            numcodes+1, numcodes*2)
     
     #IF NO GROUP VARIABLE
   } else {
@@ -192,14 +196,14 @@ trprobs <- function(d, lagvar, laggroup=NULL, title="Lag Sequential Descriptive 
     lagout %>% 
       knitr::kable(caption = paste0(title, ", Lag = ", lagnum), digits=2, format = "html", escape = F, align=rep('c', numcodes), col.names = c(rep(codenames,1))) %>%
       kableExtra::kable_styling(bootstrap_options = "striped", full_width = FALSE)  %>%
-      kableExtra::group_rows("Observed Frequencies\n(Expected Frequencies)", 1, numcodes) %>%
-      kableExtra::group_rows("Standardized Residuals\n(Transitional Probablities)", numcodes+1, numcodes*2)
+      kableExtra::pack_rows("Observed Frequencies\n(Expected Frequencies)", 1, numcodes) %>%
+      kableExtra::pack_rows("Transitional Probablities\n(Standardized Residuals)", numcodes+1, numcodes*2)
   }
   #End function 
 }
 
 
-#' Estimates log linear models for sequential data
+#' Estimates log linear models for stationarity of sequential data
 #' Requires a vector of sequential data
 #' Can request estimates by group
 #' @param d dataframe containing sequential vector
@@ -210,14 +214,14 @@ trprobs <- function(d, lagvar, laggroup=NULL, title="Lag Sequential Descriptive 
 #' @return Table with log linear estimates for lag sequential data
 #' @export
 
-lagmodels <- function(d, lagcol, laggroup, title="Lag Sequential Log Linear Models", lagnum=1) {
+lagmodels <- function(d, lagcol, laggroup="", title="Log Linear Models for Stationarity", lagnum=1) {
   
   options(scipen = 999, warn = -1)
   
   if(length(lagcol)==0) {stop("The variable does not exist in your data frame")}
   
   
-  #create data frame wiht lags = lagnum
+  #create data frame with lags = lagnum
   #https://gist.github.com/drsimonj/2038ff9f9c67063f384f10fac95de566
   lags <- seq(lagnum)
   lag_names <- paste("lag", formatC(lags, width = nchar(max(lags)), flag = "0"), 
@@ -303,9 +307,199 @@ lagmodels <- function(d, lagcol, laggroup, title="Lag Sequential Log Linear Mode
                                  "Residual Deviance df",
                                  "Residual Deviance",
                                  "p value")) %>%
-      kableExtra::kable_styling(full_width = F) %>%
-      kableExtra::collapse_rows(columns = 1:length(laggroup), valign = "top")
+      kableExtra::kable_styling(full_width = F)
+      #kableExtra::collapse_rows(columns = 1:length(laggroup), valign = "top")
   }
   
   #end function
 }
+
+
+#' Tests stationarity/homogeneity across groups for sequential data
+#' Requires a vector of sequential data
+#' Requires a group variable
+#' Tests first half of discussion against second half
+#' @param d dataframe containing sequential vector
+#' @param lagcol vector in dataframe containing sequential vector
+#' @param laggroup vector in dataframe containing grouping variable.  Default is empty (no groups)
+#' @param lagnum number of lags. Default is 1
+#' @param title caption for table
+#' @return Table with log linear estimates for lag sequential data
+#' @export
+
+shmodels <- function(d, lagcol, laggroup="", title="Homogeniety Tests for Log Linear Models", lagnum=1, st=1) {
+
+  #https://data.library.virginia.edu/an-introduction-to-loglinear-models/
+  
+  options(scipen = 999, warn = -1)
+  
+  #make sure sequential data column exists
+  if(length(d[[lagcol]])==0) {stop("The variable does not exist in your data frame")}
+  
+  #create data frame with lags = lagnum
+  #https://gist.github.com/drsimonj/2038ff9f9c67063f384f10fac95de566
+  lags <- seq(lagnum)
+  lag_names <- paste("lag", formatC(lags, width = nchar(max(lags)), flag = "0"), 
+                     sep = "")
+  lag_functions <- setNames(paste("dplyr::lag(., ", lags, ")"), lag_names)
+  
+  #define magrittr pipe
+  `%>%` <- magrittr::`%>%`
+  
+  if(missing(laggroup)) {
+  
+    print("You made it this far")
+    lag.counts <- d %>%
+      dplyr::select(lagcol) %>%
+      dplyr::mutate(time12 = ifelse(row_number() < n()/2, 1,2)) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate_at(dplyr::vars(lagcol), dplyr::funs_(lag_functions)) %>%
+      dplyr::rename(lag0 = lagcol) %>%
+      dplyr::group_by_all() %>%
+      dplyr::summarise(freq = n()) %>%
+      dplyr::ungroup() %>%
+      as.data.frame()
+    
+    lag.counts %>%
+      dplyr::select(-freq, -time12) -> lag.names
+    
+    post.form <- paste(colnames(lag.names), collapse  = ":")
+    print(post.form)
+    
+    post0.form <- paste(colnames(lag.names[-length(lag.names)]), collapse  = ":")
+    print(post0.form)
+    
+    pre.form <- paste("freq", paste(colnames(lag.names), collapse="*"), sep="~")
+    print(pre.form)
+    
+    lag.form <- as.formula(paste0(pre.form, "*time12", " - ",post.form,":time12"))
+    print(lag.form)
+    
+    ant.form <- paste0("freq", "~", post.form, " + ", post0.form, ":time12")
+    
+    print(ant.form)
+    
+    model.form <- as.formula(ant.form)
+    print(model.form)
+
+    options(knitr.kable.NA = '')
+    #https://cran.r-project.org/web/packages/broom/vignettes/broom_and_dplyr.html
+    lag.counts %>%
+      na.omit() %>%
+        glm(model.form, family = poisson, data = .) -> fit.out
+    
+    lag.counts %>%
+      na.omit() %>%
+        glm(lag.form, family = poisson, data = .) %>%
+        anova(., test = "Chisq") -> stats.out
+    
+    broom::tidy(stats.out) %>%
+      knitr::kable(digits = 2, escape = F, format="html", 
+                   col.names = c(
+                                 "Model",
+                                 "Model df",
+                                 "Deviance",
+                                 "Residual Deviance df",
+                                 "Residual Deviance",
+                                 "p value")) %>%
+      kableExtra::kable_styling(full_width = F) %>%
+      kableExtra::add_footnote(paste0("Note: Model deviance = ", 
+                                      round(broom::glance(fit.out)$deviance,2),", p = ", 
+                                      round(pchisq(broom::glance(fit.out)$deviance,
+                                             df = broom::glance(fit.out)$df.residual, 
+                                             lower.tail = F),2),". See Poole (2000)."))
+    #ends non-group function
+    
+    } else {
+  
+      #Make sure grouping variable exists  
+  if(length(d[[laggroup]])==0) {stop("The group variable does not exist in your data frame")}
+      
+  #handles a weird quoting issue with multiple calls to
+  #the same var in dplyr
+  laggroup2 <- ensyms(laggroup)
+
+    lag.counts <- d %>%
+      dplyr::select(lagcol, laggroup) %>%
+      #add time to df if stat model requested
+      dplyr::group_by(!!!laggroup2) %>%
+      dplyr::mutate(time12 = ifelse(row_number() < n()/2, 1,2)) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate_at(dplyr::vars(lagcol), dplyr::funs_(lag_functions)) %>%
+      dplyr::rename(lag0 = lagcol) %>%
+      dplyr::group_by_all() %>%
+      dplyr::summarise(freq = n()) %>%
+      dplyr::ungroup() %>%
+      as.data.frame()
+      
+    #print(length(laggroup))
+     
+    lag.counts %>%
+      dplyr::select(-freq, -laggroup, -time12) -> lag.names
+    
+    post.form <- paste(colnames(lag.names), collapse  = ":")
+    print(post.form)
+    
+    post0.form <- paste(colnames(lag.names[-length(lag.names)]), collapse  = ":")
+    print(post0.form)
+    
+    pre.form <- paste("freq", paste(colnames(lag.names), collapse="*"), sep="~")
+    print(pre.form)
+    
+    lag.form <- as.formula(paste0(pre.form, "*time12", " - ",post.form,":time12"))
+    print(lag.form)
+    
+    ant.form <- paste0("freq", "~", post.form, " + ", post0.form, ":time12")
+    
+    print(ant.form)
+    
+    model.form <- as.formula(ant.form)
+    print(model.form)
+    
+    
+    options(knitr.kable.NA = '')
+    #https://cran.r-project.org/web/packages/broom/vignettes/broom_and_dplyr.html
+    lag.counts %>%
+      dplyr::arrange_at(dplyr::vars(dplyr::one_of(laggroup))) %>%
+      na.omit() %>%
+      tidyr::nest(-laggroup) %>%
+      dplyr::mutate(
+        model.fit = purrr::map(data, ~ glm(model.form, family = poisson, data = .x)),
+        fit.glanced = purrr::map(model.fit, broom::glance),
+        model.stats = purrr::map(data, ~ glm(lag.form, family = poisson, data = .x) %>% 
+                          anova(., test = "Chisq") %>%
+                            broom::tidy(.))) -> test.out
+
+    test.out %>%
+      tidyr::unnest(model.stats, .drop=T) -> stats.out
+    
+    test.out %>%
+     tidyr::unnest(fit.glanced, .drop=T) %>%
+      dplyr::mutate(model.p = pchisq(deviance, df = df.residual, lower.tail = F)) %>%
+      dplyr::mutate(devp.mod = paste0(round(deviance,2),"<br>(p. = ",round(model.p),2,")")) %>%
+      dplyr::select(laggroup, devp.mod) -> fit.out
+
+    #print(dplyr::left_join(fit.out, stats.out))
+    #stop()
+    
+        
+    dplyr::left_join(fit.out, stats.out) %>%
+      knitr::kable(digits = 2, escape = F, format="html", 
+                   col.names = c(laggroup,
+                                 "Model Deviance",
+                                 "Model",
+                                 "Model df",
+                                 "Deviance",
+                                 "Residual Deviance df",
+                                 "Residual Deviance",
+                                 "p value")) %>%
+      kableExtra::kable_styling(full_width = F) %>%
+      kableExtra::collapse_rows(columns = 1:2, valign = "top") %>%
+      kableExtra::add_footnote("Note: Model deviance is the test for stationarity as described in Poole (2000)")
+
+    #ends group function
+    }
+    
+  #end function
+}
+
